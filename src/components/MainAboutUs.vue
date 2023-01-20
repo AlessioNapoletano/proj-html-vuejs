cb62680903875a7153e6a3c262cd05aed6b2598f
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-01-20 12:44:30 +0100

    Fix MainAboutUs Structure, padding and color

--- src/components/MainAboutUs.vue
+++ src/components/MainAboutUs.vue
@@ -6,34 +6,40 @@ export default {
 
 <template>
 
-    <section class="about-us d-flex">
-        <div class="left-box"></div>
+    <section class="about-us">
+        <div class="container-sm d-flex">
+            <div class="left-box"></div>
 
-        <div class="right-box d-flex align-center justify-center">
-            <div class="slogan">
-                <div class="small-text d-flex">
-                    <div class="line"></div>
-                    <h3>
-                        ABOUT US
-                    </h3>
-                </div>
+            <div class="right-box d-flex align-center justify-center">
+                <div class="slogan">
+                    <div class="small-text d-flex">
+                        <div class="line"></div>
+                        <h3>
+                            ABOUT US
+                        </h3>
+                    </div>
 
-                <div class="big-text">
-                    <h1>
-                        WE MAKE YOUR STRATEGY WORK
-                    </h1>
-                </div>
+                    <div class="big-text">
+                        <h1>
+                            WE MAKE YOUR STRATEGY WORK
+                        </h1>
+                    </div>
 
-                <div class="description">
-                    <p>
-                        When, while lovely valley teems with vapour around meand eridian sun strikes the upper
-                        impenetrable foliage of my trees, and but a thousand unknow plants are noticed when i hear the
-                        buzz of the little world among the stalks, and grow familiar indescribable forms of the insects
-                        and flies, then i feel the presence of the almighty .
-                    </p>
+                    <div class="description">
+                        <p>
+                            When, while lovely valley teems with vapour around meand eridian sun strikes the upper
+                            impenetrable foliage of my trees, and but a thousand unknow plants are noticed when i hear
+                            the
+                            buzz of the little world among the stalks, and grow familiar indescribable forms of the
+                            insects
+                            and flies, then i feel the presence of the almighty .
+                        </p>
+                    </div>
                 </div>
             </div>
+
         </div>
+
 
     </section>
 
@@ -43,50 +49,52 @@ export default {
 @use '../styles/partials/variables' as *;
 
 section {
-    height: 55rem;
+
     color: $main-white-color;
 
-    .left-box {
-        width: 58%;
-        height: 100%;
+    .container-sm {
+        height: 50rem;
 
-        background-image: url('../assets/images/business-people-working-together-on-project-and-5FHSKBL.jpg');
-        background-repeat: no-repeat;
-    }
+        .left-box {
+            width: 60%;
+            height: 100%;
 
-    .right-box {
-        width: 42%;
-        height: 100%;
-        padding-left: 3rem;
-        padding-right: 3rem;
+            background-image: url('../assets/images/business-people-working-together-on-project-and-5FHSKBL.jpg');
+            background-repeat: no-repeat;
+        }
 
-        .slogan {
-            width: 80%;
-            height: 50%;
+        .right-box {
+            width: 40%;
+            height: 100%;
+            padding-left: 3rem;
+            padding-right: 3rem;
+
+            .slogan {
+                width: 80%;
+                height: 50%;
+            }
+        }
+
+        .small-text {
+            margin-bottom: 1rem;
+
+            .line {
+                margin: .9rem 1rem 0 0;
+                height: 0;
+                width: 50px;
+                border: 2px solid $line-light-blue;
+            }
+        }
+
+        .big-text {
+            font-size: 2.45rem;
+            margin-bottom: 2rem;
+        }
+
+        .description {
+            color: $main-grey-color;
+            font-size: 1.1rem;
         }
     }
-
-    .small-text {
-        margin-bottom: 1rem;
-
-        .line {
-            margin: .9rem 1rem 0 0;
-            height: 0;
-            width: 50px;
-            border: 2px solid $line-light-blue;
-        }
-    }
-
-    .big-text {
-        font-size: 2.8rem;
-        margin-bottom: 2rem;
-    }
-
-    .description {
-        color: $main-grey-color;
-        font-size: 1.3rem;
-    }
-
-
 }
 </style>
